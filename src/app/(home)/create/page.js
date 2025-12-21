9fb6fda1d1d09e913c1a560c617da31f67656d70
Poet <946261953@qq.com>
2025-12-21 23:05:42 +0800

    feat：添加ui

--- src/app/(home)/create/page.js
+++ src/app/(home)/create/page.js
@@ -24,6 +24,7 @@ const App = () => {
         setLoading(true);
         optimize({ context: text })
             .then((res) => {
+                setResult(res.data);
                 console.log(res);
             })
             .catch((err) => {
