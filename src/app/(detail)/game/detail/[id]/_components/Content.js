ea7b81086c2dd1663b51fab96b76a7b8a2b07f73
Poet <1471047476@qq.com>
2026-01-08 18:35:59 +0800

    feat: 添加游戏的开发

---
+++ src/app/(detail)/game/detail/[id]/_components/Content.js
@@ -0,0 +1,111 @@
+"use client";
+import AudioPlayer from "@/app/(detail)/course/detail/_component/AudioPlayer.js";
+import { useEffect, useRef, useState } from "react";
+const App = ({ contentData, audioSrc, subTitleDate = [] }) => {
+    const audioPlayRef = useRef(null);
+    const [sentenceIndex, setSentenceIndex] = useState(0);
+    const [wordIndex, setWordIndex] = useState(null);
+    const [writeWord, setWriteWord] = useState([]);
+    const inputRef = useRef(null);
+
+    const skipSentenceWordIndex = (index, plus) => {
+        const item = subTitleDate[sentenceIndex][index];
+
+        if(item.boundaryType === 'WordBoundary') {
+            return index
+        } else  {
+            return skipSentenceWordIndex(index + plus);
+        }
+    }
+    useEffect(() => {
+        const keydown = (e) => {
+            if(document.activeElement !== inputRef.current) {
+                return;
+            }
+            // e.preventDefault();
+            if(e.key === "Enter") {
+                if(wordIndex < contentData.length - 1) {
+                    const newIndex = skipSentenceWordIndex(wordIndex + 1, 1);
+                    inputRef.current.value = writeWord[sentenceIndex][newIndex] || ""
+                    setWordIndex(newIndex);
+                }
+            } else if(e.key === "Backspace") {
+                if(wordIndex > 0 && !inputRef.current.value) {
+                    const newIndex = skipSentenceWordIndex(wordIndex - 1, -1);
+                    inputRef.current.value = writeWord[sentenceIndex][newIndex]
+                    setWordIndex(newIndex);
+                }
+            }
+        }
+        document.addEventListener("keydown", keydown);
+
+        return () => {
+            document.removeEventListener("keydown", keydown);
+        };
+    }, [wordIndex, writeWord]);
+
+    // console.log(contentData, subTitleDate);
+    const onTimeUpdate = () => {};
+    const handleInput = (e) => {
+        const word = e.target.value;
+        // writeWord[sentenceIndex][wordIndex].push(word);
+        writeWord[sentenceIndex][wordIndex] = word
+
+        setWriteWord([...writeWord]);
+    };
+    const handleClickWord = (item, index) => {
+        if (!writeWord[sentenceIndex]) {
+            writeWord[sentenceIndex] = [];
+            // setWriteWord([...writeWord])
+        }
+        setWordIndex(index);
+        inputRef.current.value = writeWord[sentenceIndex][index] || "";
+        inputRef.current.focus();
+    };
+    return (
+        <>
+            <div className="relative flex flex-wrap justify-center gap-2 sentent-wrap">
+                {subTitleDate[sentenceIndex]?.map((item, index) => {
+                    return item.boundaryType === "WordBoundary" ? (
+                        <div
+                            key={index}
+                            className={`item-word border-border flex items-center justify-center px-2.5 py-2 ${
+                                wordIndex === index ? "active" : ""
+                            }`}
+                            onClick={() => handleClickWord(item, index)}
+                            // style={{minWidth: ''}}
+                        >
+                            <span>
+                                {writeWord[sentenceIndex] &&
+                                writeWord[sentenceIndex][index]
+                                    ? writeWord[sentenceIndex][index]
+                                    : null}
+                            </span>
+                        </div>
+                    ) : (
+                        <div
+                            key={index}
+                            className="ml-3"
+                            dangerouslySetInnerHTML={{ __html: item.text }}
+                        ></div>
+                    );
+                })}
+                <input
+                    ref={inputRef}
+                    className="w-full h-full absolute opacity-0"
+                    style={{ zIndex: -1 }}
+                    onInput={handleInput}
+                ></input>
+            </div>
+            {audioSrc && (
+                <AudioPlayer
+                    onTimeUpdate={onTimeUpdate}
+                    src={audioSrc}
+                    ref={audioPlayRef}
+                />
+            )}
+        </>
+    );
+};
+
+export default App;
---
+++ src/app/(detail)/game/detail/[id]/page.js
@@ -0,0 +1,46 @@
+import { getArticleId } from "@/api/server/course";
+import { notFound } from "next/navigation";
+import he from "he";
+import Content from "./_components/Content";
+import './_components/page.scss'
+export async function generateMetadata({ params }) {
+    const id = (await params).id;
+    let res = await getArticleId(id);
+
+    if (!res.data) {
+        return { title: "课程不存在" };
+    }
+    const decodedTitle = he.decode(res.data.title);
+    return {
+        title: `${decodedTitle} - ID: ${id}`,
+        description: `这是关于课程 ${id} 的详细介绍`,
+    };
+}
+const App = async ({ params }) => {
+    const { id } = await params; // params 已经解构，不需要再次 await
+    const BASE_URL = process.env.API_URL;
+    let res = await getArticleId(id);
+
+    if (!res.data) {
+        notFound();
+    }
+    // 构造完整的音频 URL
+    const audioSrc = res.data.url
+        ? BASE_URL + "/" + res.data.url.split(".")[0] + "/index.m3u8"
+        : "";
+    return (
+        <>
+            <h1
+                className="text-center pb-2 font-bold text-[18px] pt-2.5"
+                dangerouslySetInnerHTML={{ __html: res.data.title }}
+            />
+            <Content
+                audioSrc={audioSrc}
+                contentData={res.data.content}
+                subTitleDate={res.data.subtitle}
+            />
+        </>
+    );
+};
+
+export default App;
