d104eda71efe32589f4cf9f88853f603641648ea
Poet <946261953@qq.com>
2026-01-19 11:40:58 +0800

    feat: 添加多语言

--- src/app/[locale]/(auth)/register/_components/Code.js
+++ src/app/[locale]/(auth)/register/_components/Code.js
@@ -1,29 +1,33 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 import { isEmailSimple } from "@/lib/validate";
 import { useState, useEffect } from "react";
-import { sendCode } from '@/api/login'
-
+import { sendCode } from "@/api/login";
+import { useTranslations } from "next-intl";
 
 const App = ({ value }) => {
+    const t = useTranslations("register");
     const [count, setCount] = useState(0);
     const [loading, setLoading] = useState(false);
     const clickHandler = () => {
         if (!value) {
-            toast.error("请输入邮箱");
+            // toast.error("请输入邮箱");
+            toast.error(t("errors.emailRequired"));
             return;
         } else if (!isEmailSimple(value)) {
-            toast.error("请输入正确的邮箱");
-            return
+            // toast.error("请输入正确的邮箱");
+            toast.error(t("errors.emailInvalid"));
+            return;
         }
 
         setLoading(true);
         sendCode({ email: value })
             .then(() => {
-                setCount(90)
-                toast.success("验证码已发送，请查收");
+                setCount(90);
+                // toast.success("验证码已发送，请查收");
+                toast.success(t("sendCode.success"));
             })
             .finally(() => {
                 setLoading(false);
@@ -46,7 +50,6 @@ const App = ({ value }) => {
     }, [count]);
     return (
         <>
-            
             <Button
                 type="button"
                 className="ml-2"
@@ -54,7 +57,10 @@ const App = ({ value }) => {
                 loading={loading}
                 disabled={count > 0}
             >
-                {count > 0 ? `已发送(${count})` : '发送验证码'}
+                {/* {count > 0 ? `已发送(${count})` : '发送验证码'} */}
+                {count > 0
+                    ? t("sendCode.button.sent", { count: count })
+                    : t("sendCode.button.send")}
             </Button>
         </>
     );
